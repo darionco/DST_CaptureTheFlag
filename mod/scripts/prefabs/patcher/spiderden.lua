fbb871af7894542050b174f236e10a2904c4cef4
Dario Segura <darionco.dev@gmail.com>
2021-01-21 00:34:02 -0500

    [SPIDERS] Better code structure

--- mod/scripts/prefabs/patcher/spiderden.lua
+++ mod/scripts/prefabs/patcher/spiderden.lua
@@ -4,35 +4,41 @@
 --- DateTime: 2021-01-17 1:43 p.m.
 ---
 
+local patchSetStageFunctions = function(inst)
+    local OldSetSmall = inst.SetSmall;
+    inst.SetSmall = function(den)
+        OldSetSmall(den);
+        den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget'});
+    end
+
+    local OldSetMedium = inst.SetMedium;
+    inst.SetMedium = function(den)
+        OldSetMedium(den);
+        den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
+    end
+
+    local OldSetLarge = inst.SetLarge;
+    inst.SetLarge = function(den)
+        OldSetLarge(den);
+        den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
+    end
+end
+
+local patchDefaultLoot = function(inst)
+    if inst.data then
+        if inst.data.stage == 1 then
+            inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget'});
+        elseif inst.data.stage == 2 then
+            inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
+        elseif inst.data.stage == 3 then                inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
+        end
+    end
+end
+
 CTFPrefabPatcher:registerPrefabPatcher('spiderden', function(inst, data)
     if TheWorld.ismastersim then
-        local OldSetSmall = inst.SetSmall;
-        inst.SetSmall = function(den)
-            OldSetSmall(den);
-            den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget'});
-        end
-
-        local OldSetMedium = inst.SetMedium;
-        inst.SetMedium = function(den)
-            OldSetMedium(den);
-            den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
-        end
-
-        local OldSetLarge = inst.SetLarge;
-        inst.SetLarge = function(den)
-            OldSetLarge(den);
-            den.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
-        end
-
-        if inst.data then
-            if inst.data.stage == 1 then
-                inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget'});
-            elseif inst.data.stage == 2 then
-                inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
-            elseif inst.data.stage == 3 then
-                inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
-            end
-        end
+        patchSetStageFunctions(inst);
+        patchDefaultLoot(inst);
 
         if data.ctf_team then
             CTFTeamManager:registerTeamObject(inst, data);

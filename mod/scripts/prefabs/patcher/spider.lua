d7459343fd3052004185c2c775afcbbdcc8e6c21
Dario Segura <darionco.dev@gmail.com>
2021-01-23 13:04:31 -0500

    [PATCHES] Minion spiders can only attack enemy players

--- mod/scripts/prefabs/patcher/spider.lua
+++ mod/scripts/prefabs/patcher/spider.lua
@@ -3,9 +3,22 @@
 --- Created by darionco.
 --- DateTime: 2021-01-17 1:47 p.m.
 ---
+---
+local require = GLOBAL.require;
+local CTF_CONSTANTS = require('teams/CTFTeamConstants');
+local CTFTeamCombat = require('teams/CTFTeamCombat');
 
 AddPrefabPostInit('spider', function(inst)
     if TheWorld.ismastersim then
         inst.components.lootdropper:SetLoot({'goldnugget'});
+
+        local OldRetargetFunction = inst.components.combat.targetfn;
+        inst.components.combat:SetRetargetFunction(inst.components.combat.retargetperiod, function(self)
+            if self:HasTag(CTF_CONSTANTS.CTF_TEAM_MINION_TAG) then
+                local radius = self.components.knownlocations:GetLocation("investigate") ~= nil and TUNING.SPIDER_INVESTIGATETARGET_DIST or TUNING.SPIDER_TARGET_DIST;
+                return CTFTeamCombat.findEnemy(self, SpringCombatMod(radius), self.data.ctf_team_tag);
+            end
+            return OldRetargetFunction(self);
+        end)
     end
 end);

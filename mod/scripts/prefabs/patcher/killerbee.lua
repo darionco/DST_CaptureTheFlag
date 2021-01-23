b35d924118adc43460e47949eb1c2abb5d75b2f2
Dario Segura <darionco.dev@gmail.com>
2021-01-22 23:34:39 -0500

    [PATCHES] Killer bees that belong to a team only target enemy team units

--- mod/scripts/prefabs/patcher/killerbee.lua
+++ mod/scripts/prefabs/patcher/killerbee.lua
@@ -4,8 +4,20 @@
 --- DateTime: 2021-01-17 12:42 p.m.
 ---
 
+local require = GLOBAL.require;
+local CTF_CONSTANTS = require('teams/CTFTeamConstants');
+local CTFTeamCombat = require('teams/CTFTeamCombat');
+
 AddPrefabPostInit('killerbee', function(inst)
     if TheWorld.ismastersim then
         inst.components.lootdropper:SetLoot({'goldnugget'});
+
+        local OldRetargetFunction = inst.components.combat.targetfn;
+        inst.components.combat:SetRetargetFunction(inst.components.combat.retargetperiod, function(self)
+            if self:HasTag(CTF_CONSTANTS.CTF_TEAM_MINION_TAG) then
+                return CTFTeamCombat.findEnemy(self, SpringCombatMod(8), self.data.ctf_team_tag);
+            end
+            return OldRetargetFunction(self);
+        end)
     end
 end);

2c80b2ebd0662445e2934f1905e95bb832e9521c
Dario Segura <darionco.dev@gmail.com>
2021-01-24 16:33:39 -0500

    [PATCHES] Bishop is super strong

--- mod/scripts/prefabs/patcher/bishop.lua
+++ mod/scripts/prefabs/patcher/bishop.lua
@@ -11,6 +11,28 @@ CTFPrefabPatcher:registerPrefabPatcher('bishop', function(inst, data)
     if TheWorld.ismastersim then
         inst.components.lootdropper.chanceloottable = false;
         inst.components.lootdropper:SetLoot({'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget', 'goldnugget'});
+
+        if inst.components then
+            if inst.components.health then
+                inst.components.health:SetMaxHealth(800);
+                inst.components.health:SetAbsorptionAmountFromPlayer(0.5);
+            end
+
+            if inst.components.combat then
+                inst.components.combat:SetAttackPeriod(1.1);
+                inst.components.combat:SetRange(8);
+                inst.components.combat:SetDefaultDamage(100);
+            end
+
+            if inst.components.inventory then
+                local weapon = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS);
+                if weapon then
+                    weapon.components.weapon:SetDamage(90);
+                    weapon.components.weapon:SetRange(8, 14);
+                end
+            end
+        end
+
         if data.ctf_team then
             CTFTeamManager:registerTeamObject(inst, data);
         end

3830b3364c4a9ab7756ea0a1c8e3aef85ea38c34
Dario Segura <darionco.dev@gmail.com>
2021-02-21 12:44:27 -0500

    [CHARACTERS] Wolfgang balancing
    - 40% speed reduction at a full belly
    - no speed reduction at 100 hunger or less

--- mod/scripts/prefabs/characters/wolfgang.lua
+++ mod/scripts/prefabs/characters/wolfgang.lua
@@ -9,8 +9,12 @@ TUNING.WOLFGANG_ATTACKMULT_MIGHTY_MIN = 1;
 TUNING.WOLFGANG_ATTACKMULT_MIGHTY_MAX = 1;
 
 local WOLFGANG_HUNGER_THRESHOLD = 100;
+
 local WOLFGANG_DAMAGE_ABSORPTION_MIN = 0.8;
 local WOLFGANG_DAMAGE_ABSORPTION_MAX = 1.6;
+
+local WOLFGANG_WALK_SPEED_MULT_MIN = 0.6;
+local WOLFGANG_WALK_SPEED_MULT_MAX = 1.0;
 
 local WOLFGANG_SPAWN_HUNGER = 200;
 
@@ -25,16 +29,17 @@ AddPrefabPostInit('wolfgang', function(inst)
         OldApplyScale(f_inst, source, scale);
         if f_inst.components and f_inst.components.locomotor then
             if scale > 1 then
-                f_inst.components.locomotor:SetExternalSpeedMultiplier(f_inst, "ctf_anti_mightiness", 1 / scale);
+                f_inst.components.locomotor:SetExternalSpeedMultiplier(f_inst, 'ctf_anti_mightiness', 1 / scale);
             else
-                f_inst.components.locomotor:RemoveExternalSpeedMultiplier(f_inst, "ctf_anti_mightiness");
+                f_inst.components.locomotor:RemoveExternalSpeedMultiplier(f_inst, 'ctf_anti_mightiness');
             end
         end
     end
 
     if inst.components and inst.components.health and inst.components.hunger then
-        local absorptionDomain = WOLFGANG_DAMAGE_ABSORPTION_MAX - WOLFGANG_DAMAGE_ABSORPTION_MIN;
         local hungerDomain = inst.components.hunger.max - WOLFGANG_HUNGER_THRESHOLD;
+
+        local absorptionDomain = WOLFGANG_DAMAGE_ABSORPTION_MAX - WOLFGANG_DAMAGE_ABSORPTION_MIN;
         local OldDoDelta = inst.components.health.DoDelta;
         inst.components.health.DoDelta = function (self, amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb)
             local hungerMult = math.max(0, inst.components.hunger.current - WOLFGANG_HUNGER_THRESHOLD) / hungerDomain;
@@ -43,5 +48,12 @@ AddPrefabPostInit('wolfgang', function(inst)
             amount = amount * absorptionMult;
             return OldDoDelta(self, amount, overtime, cause, ignore_invincible, afflicter, ignore_absorb);
         end
+
+        local walkSpeedDomain = WOLFGANG_WALK_SPEED_MULT_MAX - WOLFGANG_WALK_SPEED_MULT_MIN;
+        inst:ListenForEvent('hungerdelta', function(f_inst, data, forcesilent)
+            local hungerMult = math.max(0, f_inst.components.hunger.current - WOLFGANG_HUNGER_THRESHOLD) / hungerDomain;
+            local speedMult = WOLFGANG_WALK_SPEED_MULT_MAX - walkSpeedDomain * hungerMult;
+            f_inst.components.locomotor:SetExternalSpeedMultiplier(f_inst, 'ctf_mightiness_slowdown', speedMult);
+        end);
     end
 end);

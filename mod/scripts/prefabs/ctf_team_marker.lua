32a09bacf37ef2ea9501d22826bd5920605782a0
Dario Segura <darionco.dev@gmail.com>
2021-03-05 15:54:28 -0500

    [PREFABS] Team marker only does basic configuration

--- mod/scripts/prefabs/ctf_team_marker.lua
+++ mod/scripts/prefabs/ctf_team_marker.lua
@@ -12,23 +12,19 @@ local function ctf()
     local inst = CreateEntity();
 
     inst.entity:AddTransform();
-    inst.entity:AddFollower();
     inst.entity:AddAnimState();
     inst.entity:AddNetwork();
+    inst.entity:AddFollower();
     inst:AddTag('FX');
-
-    inst.Transform:SetPosition(0, -0.05, 0);
 
     inst.AnimState:SetBank('ctf_team_marker');
     inst.AnimState:SetBuild('ctf_team_marker');
     inst.AnimState:PlayAnimation('idle');
-    inst.AnimState:SetMultColour(1, 1, 1, 1);
     inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround);
 
-    inst.entity:SetPristine();
     inst.persists = false;
 
     return inst;
 end
 
-return Prefab( 'ctf_team_marker', ctf, assets);
+return Prefab('ctf_team_marker', ctf, assets);

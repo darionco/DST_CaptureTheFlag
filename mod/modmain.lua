00941ed4b180609d4d0e6867aaeff6bdf838b8dc
Dario Segura <darionco.dev@gmail.com>
2021-03-05 20:49:08 -0500

    [DEBUG] Networking

--- mod/modmain.lua
+++ mod/modmain.lua
@@ -3,22 +3,22 @@ local CTF_TEAM_CONSTANTS = require('constants/CTFTeamConstants');
 
 modimport('use');
 
-modimport('scripts/teams/CTFTeamManager');
-modimport('scripts/teams/CTFTeam');
-modimport('scripts/teams/CTFPlayer');
-
-modimport('scripts/init/screens');
-modimport('scripts/init/assets');
-modimport('scripts/init/prefab_on_load');
-modimport('scripts/init/crafting');
-modimport('scripts/init/crafting_descriptions');
-modimport('scripts/init/loot');
-modimport('scripts/init/food');
-modimport('scripts/init/characters');
-modimport('scripts/init/character_descriptions');
-modimport('scripts/init/player');
-modimport('scripts/init/chat');
-modimport('scripts/init/network');
+--modimport('scripts/teams/CTFTeamManager');
+--modimport('scripts/teams/CTFTeam');
+--modimport('scripts/teams/CTFPlayer');
+--
+--modimport('scripts/init/screens');
+--modimport('scripts/init/assets');
+--modimport('scripts/init/prefab_on_load');
+--modimport('scripts/init/crafting');
+--modimport('scripts/init/crafting_descriptions');
+--modimport('scripts/init/loot');
+--modimport('scripts/init/food');
+--modimport('scripts/init/characters');
+--modimport('scripts/init/character_descriptions');
+--modimport('scripts/init/player');
+--modimport('scripts/init/chat');
+--modimport('scripts/init/network');
 
 
 local function handlePlayerJoined(world, player)
@@ -27,7 +27,7 @@ end
 
 local function handlePlayerDisconnected(world, args)
     world:PushEvent(CTF_TEAM_CONSTANTS.PLAYER_DISCONNECTED_EVENT, args.player);
-    CTFTeamManager:removePlayer(args.player);
+    --CTFTeamManager:removePlayer(args.player);
 end
 
 local function handlePlayerSpawn(world, player)
@@ -42,13 +42,13 @@ AddPrefabPostInit('world', function(inst)
     inst:ListenForEvent('ms_playerdisconnected', handlePlayerDisconnected);
     inst:ListenForEvent("ms_playerspawn", handlePlayerSpawn);
 
-    -- check world completeness
-    if inst.ismastersim then
-        inst:DoTaskInTime(0, function()
-            local team = CTFTeamManager:getTeamWithLeastPlayers();
-            if not team then
-                c_regenerateworld();
-            end
-        end);
-    end
+    ---- check world completeness
+    --if inst.ismastersim then
+    --    inst:DoTaskInTime(0, function()
+    --        local team = CTFTeamManager:getTeamWithLeastPlayers();
+    --        if not team then
+    --            c_regenerateworld();
+    --        end
+    --    end);
+    --end
 end);

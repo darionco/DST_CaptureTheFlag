7065d70eeddf1415a980d5a1569fa27c616df4b3
Dario Segura <darionco.dev@gmail.com>
2021-03-06 01:12:44 -0500

    [DEBUG] Networking

--- mod/scripts/prefabs/ctf_team_marker.lua
+++ mod/scripts/prefabs/ctf_team_marker.lua
@@ -10,39 +10,52 @@ local assets = {
 
 local function ctf()
     print('============================================ ctf_team_marker');
+    --local inst = CreateEntity();
+    --
+    --inst.entity:AddTransform();
+    --inst.entity:AddAnimState();
+    --inst.entity:AddNetwork();
+    --inst.entity:AddFollower();
+    ----inst:AddTag('FX');
+    --inst:AddTag("CLASSIFIED");
+    --
+    --inst.AnimState:SetBank('ctf_team_marker');
+    --inst.AnimState:SetBuild('ctf_team_marker');
+    --inst.AnimState:PlayAnimation('idle');
+    --inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround);
+    --
+    --inst.ctf_owner = net_entity(inst.GUID, 'ctf_owner', 'ctf_owner');
+    --inst.ctf_health = net_float(inst.GUID, 'ctf_health', 'ctf_health');
+    --
+    ----if not TheNet:IsDedicated() then
+    ----    inst:ListenForEvent(inst.user_id.event, function()
+    ----        CTFPlayer(inst);
+    ----    end);
+    ----end
+    --
+    --if TheWorld.ismastersim then
+    --    inst:DoTaskInTime(0, function()
+    --        local owner = inst.entity:GetParent();
+    --        print('============================= ctf_team_marker owner:', owner);
+    --        inst.ctf_owner:set(owner);
+    --        if owner then
+    --            -- do something
+    --        end
+    --    end);
+    --end
+    --
+    --inst.entity:SetPristine();
+    --inst.persists = false;
+
     local inst = CreateEntity();
 
     inst.entity:AddTransform();
-    inst.entity:AddAnimState();
     inst.entity:AddNetwork();
-    inst.entity:AddFollower();
-    --inst:AddTag('FX');
+    inst.entity:SetCanSleep(false);
     inst:AddTag("CLASSIFIED");
-
-    inst.AnimState:SetBank('ctf_team_marker');
-    inst.AnimState:SetBuild('ctf_team_marker');
-    inst.AnimState:PlayAnimation('idle');
-    inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround);
 
     inst.ctf_owner = net_entity(inst.GUID, 'ctf_owner', 'ctf_owner');
     inst.ctf_health = net_float(inst.GUID, 'ctf_health', 'ctf_health');
-
-    --if not TheNet:IsDedicated() then
-    --    inst:ListenForEvent(inst.user_id.event, function()
-    --        CTFPlayer(inst);
-    --    end);
-    --end
-
-    if TheWorld.ismastersim then
-        inst:DoTaskInTime(0, function()
-            local owner = inst.entity:GetParent();
-            print('============================= ctf_team_marker owner:', owner);
-            inst.ctf_owner:set(owner);
-            if owner then
-                -- do something
-            end
-        end);
-    end
 
     inst.entity:SetPristine();
     inst.persists = false;

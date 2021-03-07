19f6c818ccc1e2db325de3684944193ccda7e097
Dario Segura <darionco.dev@gmail.com>
2021-03-07 01:19:59 -0500

    [PREFABS] Spider pillars cannot be killed with ranged weapons

--- mod/scripts/prefabs/patcher/wall_ruins_2.lua
+++ mod/scripts/prefabs/patcher/wall_ruins_2.lua
@@ -9,6 +9,15 @@ local CTF_TEAM_CONSTANTS = require('constants/CTFTeamConstants');
 CTFPrefabPatcher:registerPrefabPatcher('wall_ruins_2', function(inst, data)
     if TheWorld.ismastersim then
         if data.ctf_team then
+            if inst.components and inst.components.combat then
+                local OldGetAttacked = inst.components.combat.GetAttacked;
+                inst.components.combat.GetAttacked = function(self, attacker, damage, weapon, stimuli)
+                    if weapon and weapon:HasTag('projectile') then
+                        damage = 0;
+                    end
+                    return OldGetAttacked(self, attacker, damage, weapon, stimuli);
+                end
+            end
             CTFTeamManager:registerTeamObject(inst, data);
         end
     end

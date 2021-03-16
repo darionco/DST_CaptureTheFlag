376575f1365766bb7a4e6cd042ebed1daf603463
JayLil07 <julia.seag@gmail.com>
2021-03-15 22:05:51 -0400

    [CHARACTERS] updated character descriptions

--- mod/scripts/init/character_descriptions.lua
+++ mod/scripts/init/character_descriptions.lua
@@ -27,12 +27,12 @@ end
 
 STRINGS.CHARACTER_DESCRIPTIONS.wilson = 'I shall take your life force and use it to grow my stubble'; -- 'WORK IN PROGRESS'; -- '\n*Is a well rounded character';
 STRINGS.CHARACTER_DESCRIPTIONS.willow = 'WORK IN PROGRESS'; -- '*Can deploy bernie to battle';
-STRINGS.CHARACTER_DESCRIPTIONS.wendy = 'WORK IN PROGRESS'; -- '*Can place Abigail somewhere on the map\n*Abigail cannot move';
-STRINGS.CHARACTER_DESCRIPTIONS.wolfgang = '*Absorbs up to 60% damage when full\n*Gets slower on a full stomach\n*Is a literal tank';
+STRINGS.CHARACTER_DESCRIPTIONS.wendy = '*Can use Abigail for a quick burst of damage\n*Abigails health becomes her damage\n*Resets after each use';
+STRINGS.CHARACTER_DESCRIPTIONS.wolfgang = '*Absorbs up to 60% damage when full\n*Gets slower on a full stomach\n*Takes 20% more damage when hungry\n*Gets faster when hungry\n*Is a literal tank';
 STRINGS.CHARACTER_DESCRIPTIONS.wx78 = '*Can buy gears to upgrade from the store\n*Can get very tanky';
-STRINGS.CHARACTER_DESCRIPTIONS.wickerbottom = '*Can craft books to help her team\n*One restores 45 health and 35 hunger\n*One harms enemies by 50% and allies by 25%\n*Both apply to structures';
-STRINGS.CHARACTER_DESCRIPTIONS.wes = '*Is a glass cannon\n*Armor effectiveness reduced bt 75%\n*Has 55% damage multiplier\n*75 points of everything';
-STRINGS.CHARACTER_DESCRIPTIONS.waxwell = '*Can spawn up to 5 shadow duelists';
+STRINGS.CHARACTER_DESCRIPTIONS.wickerbottom = '*Can craft books to help her team\n*One restores 150 health and 60 hunger\n*One harms enemies by 30% and allies by 20%\n*Both apply to structures';
+STRINGS.CHARACTER_DESCRIPTIONS.wes = '*Armor effectiveness reduced by 50%\n*Does 50% more damage\n*All damage taken does at least 5 damage\n*Or at most 50% of his currnt health\n*Has 75 health, hunger and sanity';
+STRINGS.CHARACTER_DESCRIPTIONS.waxwell = '*Can spawn up to 5 shadow duelists\n*Each duelist has 150 health\n*And does 40 damage';
 STRINGS.CHARACTER_DESCRIPTIONS.woodie = '*Can transform into the weremoose';
 STRINGS.CHARACTER_DESCRIPTIONS.woodie_us = '*Can transform into the weremoose\n*Will beat you in hockey';
 STRINGS.CHARACTER_DESCRIPTIONS.woodie_canada = '*Can transform into the weremoose\n*Comes from the true north\n*Oh Ca.na.daaaa...';

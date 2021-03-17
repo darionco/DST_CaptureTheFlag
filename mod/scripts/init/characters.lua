7ad19aebe8fad44168ab899374f446410dbbf286
Dario Segura <darionco.dev@gmail.com>
2021-03-16 22:46:51 -0400

    [WX78] Fixes
    - Always drops the ceiling of the gears consumed divided by half
    - Enemies can no longer pick up gears dropped after they were consumed

--- mod/scripts/init/characters.lua
+++ mod/scripts/init/characters.lua
@@ -17,6 +17,7 @@ modimport('scripts/prefabs/characters/maxwell');
 modimport('scripts/prefabs/characters/winona');
 modimport('scripts/prefabs/characters/wendy');
 modimport('scripts/prefabs/characters/woodie');
+modimport('scripts/prefabs/characters/wx78');
 
 -- disable characters not completed/banned etc
 for _, v in ipairs(CTF_MISC_CONSTANTS.DISABLED_CHARACTERS) do

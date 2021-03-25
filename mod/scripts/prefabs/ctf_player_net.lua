4693166845e1a15d77885f88a2a2d44fb6a4b2ea
Dario Segura <darionco.dev@gmail.com>
2021-03-25 00:43:06 -0400

    [PLAYER] CTFPlayer net no longer keeps track of cooldown

--- mod/scripts/prefabs/ctf_player_net.lua
+++ mod/scripts/prefabs/ctf_player_net.lua
@@ -28,9 +28,6 @@ local function ctf()
     createNetVar(inst, 'assists', net_ushortint);
     createNetVar(inst, 'bounty', net_ushortint);
 
-    createNetVar(inst, 'cooldown_charged', net_bool);
-    createNetVar(inst, 'cooldown_time', net_float);
-
     inst.entity:SetPristine();
     inst.persists = false;
 
